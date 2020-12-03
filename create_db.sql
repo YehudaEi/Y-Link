3b4ab33d6a94d1aeb1737a0a48e9eec72a2dffda
Yehuda Eisenberg <32451776+YehudaEi@users.noreply.github.com>
2020-12-03 19:06:39 +0200

    More efficient data retention

--- create_db.sql
+++ create_db.sql
@@ -5,3 +5,14 @@ CREATE TABLE `mainTable` (
     `password` TEXT NOT NULL , 
     `deleted` BOOLEAN NOT NULL , 
 PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+CREATE TABLE `clicks` (
+ `id` int NOT NULL AUTO_INCREMENT,
+ `path` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+ `ip` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+ `user_agent` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+ `language` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+ `referrer` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+ `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
